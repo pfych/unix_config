d9c55fa4066cb6af4360146ef401710842607409
pfych <contact@pfy.ch>
2022-11-11 13:30:57 +1100

    Better alpha detection

--- .config/scripts/compress.sh
+++ .config/scripts/compress.sh
@@ -1,13 +1,15 @@
-function decimate() {
+decimate() {
   ffmpeg -i "$1" -map 0:v -vf mpdecimate,setpts=N/FRAME_RATE/TB "${1%.*}"-decimated.mp4
 }
 
-function drive-by() {
+drive-by() {
   for file in *.png; do
-    if convert "$file" -format "%[opaque]" info: | grep -q "True"; then
-      convert "$file" -define png:include-chunk=none -quality 95 -depth 8 "$file"
-    else
+    if convert "$file" -channel a -separate -format "%[fx:100*mean]%" info: | grep -qE "(99\.|100)"; then
       convert "$file" "${file%.png}.jpg"
+      echo "Created jpg"
+    else
+      convert "$file" -define png:include-chunk=none -quality 95 -depth 8 "$file"
+      echo "Compressed png"
     fi
   done
 }
