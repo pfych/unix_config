921ed4ff8d868bf29fb6c214dd0125ad662f5c40
pfych <contact@pfy.ch>
2024-01-23 08:36:49 +1100

    Forgot to commit emacs changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
-(load (expand-file-name "~/.quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
+;; Set this value for startup times idk?
+(setq gc-cons-threshold 100000000)
 
 ;; Set up MELPA packages
 (require 'package)
@@ -7,7 +7,7 @@
 (package-initialize)
 
 ;; Confirm all required packages are installed
-(setq package-list '(catppuccin-theme))
+(setq package-list '(catppuccin-theme evil))
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -19,10 +19,46 @@
 ;; Setup theme
 (load-theme 'catppuccin :no-confirm)
 (setq catppucin-flavor 'mocha)
-(catppuccin-set-color 'base "#1c1c1c") ;; TODO: Could this be a loop?
-(catppuccin-set-color 'mantle "#1c1c1c")
-(catppuccin-set-color 'crust "#1c1c1c")
+
+;; set theme-option to each of the options. List has '() to avoid evaluating inner.
+(setq theme-options '(base mantle crust))
+(dolist (option theme-options)
+  (catppuccin-set-color option "#1c1c1c"))
+
 (catppuccin-reload)
+
+;; Setup slime helper for lisp
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
+
+;; Setup Evil Mode (vim shortcuts)
+(require 'evil)
+(evil-mode 1)
+
+;; Make :q and :wq close buffer/window instead of killing emacs
+(defun save-and-kill ()
+  (interactive)
+  (save-buffer)
+  (kill-buffer-and-window))
+
+(evil-ex-define-cmd "wq" 'save-and-kill)
+(evil-ex-define-cmd "q" 'kill-buffer-and-window)
+
+;; Setup org mode
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+;; Configure org mode headers
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
